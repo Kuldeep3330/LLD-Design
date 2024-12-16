8ee4caee0956eb95566be4e2f346806926f9f1b8
Kuldeep Kumar <upsainian3330@gmail.com>
2024-12-16 14:48:42 +0530

    command design pattern

---
+++ patterns/commandDesign.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+// command interface
+class Command
+{
+public:
+    virtual void execute() = 0;
+};
+// Reciever
+class Light
+{
+public:
+    void turnOn()
+    {
+        cout << "Light ON" << endl;
+    }
+    void turnOff()
+    {
+        cout << "Light OFF" << endl;
+    }
+};
+class TurnOnLightCommand : public Command
+{
+private:
+    Light light;
+
+public:
+    TurnOnLightCommand(Light light) : light(light) {}
+    void execute() override { light.turnOn(); }
+};
+
+class TurnOffLightCommand : public Command
+{
+private:
+    Light light;
+
+public:
+    TurnOffLightCommand(Light light) : light(light) {}
+    void execute() override { light.turnOff(); }
+};
+
+// invoker
+class RemoteControl
+{
+private:
+    vector<Command *> commands;
+
+public:
+    void addCommands(Command *command)
+    {
+        commands.push_back(command);
+    }
+    void pressButton()
+    {
+        if (!commands.empty())
+        {
+            commands.back()->execute();
+            commands.pop_back();
+        }
+        else
+        {
+            cout << "No more commands\n";
+        }
+    }
+};
+
+int main()
+{
+    Light roomLight;
+    // create commands
+    TurnOnLightCommand *lightOn = new TurnOnLightCommand(roomLight);
+    TurnOffLightCommand *lightOff = new TurnOffLightCommand(roomLight);
+    RemoteControl remote;
+    remote.addCommands(lightOn);
+    remote.addCommands(lightOff);
+    remote.pressButton();
+    remote.pressButton();
+    remote.pressButton();
+    return 0;
+}

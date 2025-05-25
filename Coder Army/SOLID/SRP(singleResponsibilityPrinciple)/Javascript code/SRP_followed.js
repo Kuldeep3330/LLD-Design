25681caa5c6ebc4cb955a0227ddcb1a9b5854417
Kuldeep Kumar <upsainian3330@gmail.com>
2025-05-26 00:50:55 +0530

    SRP followed in Javascript

--- Coder Army/SOLID/SRP(singleResponsibilityPrinciple)/Javascript code/SRP_followed.js
+++ Coder Army/SOLID/SRP(singleResponsibilityPrinciple)/Javascript code/SRP_followed.js
@@ -1,7 +1,75 @@
-class Product{
-    constructor(id, name,price){
-        this.id=id
-        this.name=name
-        this.price=price
-    }
+class Product {
+  constructor(id, name, price) {
+    this.id = id
+    this.name = name
+    this.price = price
+  }
 }
+
+class ShoppingCart {
+  constructor() {
+    this.products = []
+  }
+
+  addProduct(product) {
+    this.products.push(product)
+  }
+
+  removeProduct(id) {
+    const originalLength = this.products.length
+    this.products = this.products.filter((product) => product.id !== id)
+    if (this.products.length < originalLength) {
+      console.log(`Product with ID ${id} has been removed`)
+    } else {
+      console.log(`Product with ID ${id} not found`)
+    }
+  }
+
+  getProducts() {
+    return this.products
+  }
+
+  calculateTotal() {
+    return this.products.reduce((sum, product) => sum + product.price, 0)
+  }
+}
+
+class Invoice {
+  constructor(cart) {
+    this.cart = cart
+  }
+
+  printInvoice() {
+    console.log(`Your Invoice:`)
+    this.cart.getProducts().forEach((product) => {
+      console.log(`${product.name} - ₹${product.price}`)  // Fixed typo: p.price → product.price
+    })
+    console.log(`Your cart total: ₹${this.cart.calculateTotal()}`)
+  }
+}
+
+class Database {
+  constructor(cart) {
+    this.cart = cart
+  }
+
+  saveToDB() {
+    console.log("Your shopping cart has been saved to the database.")
+  }
+}
+
+// Usage
+const p1 = new Product(1, "Apple", 100)
+const p2 = new Product(2, "Milk", 60)
+const p3 = new Product(3, "Bread", 40)
+
+const cart = new ShoppingCart()
+cart.addProduct(p1)
+cart.addProduct(p2)
+cart.addProduct(p3)
+
+const invoicePrinter = new Invoice(cart)
+invoicePrinter.printInvoice()
+
+const db = new Database(cart)
+db.saveToDB()
